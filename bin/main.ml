58d18a8c3c6f3bb1439c5e4bef4866c8cef673db
Hyegeun Song <hgssong5648@naver.com>
2025-11-27 01:53:58 +0900

    a simple getter

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,19 @@
-let () = print_endline "Hello, World!"
+open! Core
+
+let base_uri_string =
+  "https://munjang.or.kr/board.es?mid=a20102000000&bid=0002&act=view&ord=B&list_no=107140&nPage=1&c_page="
+;;
+
+let uri = Uri.of_string base_uri_string
+
+let get uri =
+  let get uri =
+    let open Lwt.Syntax in
+    let* _resp, body = Cohttp_lwt_unix.Client.get uri in
+    let* body = Cohttp_lwt.Body.to_string body in
+    Lwt.return body
+  in
+  Lwt_main.run (get uri)
+;;
+
+let () = Stdio.print_endline (get uri)
